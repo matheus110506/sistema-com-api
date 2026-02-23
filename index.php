0305a331c704a75ac35d88b42195a8a71f25405f
matheus110506 <matheus110506@hotmail.com>
2026-02-23 17:00:46 -0300

    Refactor index.php to use MaeController for new routes

--- index.php
+++ index.php
@@ -2,25 +2,44 @@
 session_start();
 
 require_once "controllers/AuthController.php";
-
-$controller = new AuthController();
+require_once "controllers/MaeController.php";
 
 $page = $_GET["page"] ?? "login";
 
 switch ($page) {
 
     case "login":
-        $controller->login();
-        break;
-
-    case "dashboard":
-        $controller->dashboard();
+        (new AuthController())->login();
         break;
 
     case "logout":
-        $controller->logout();
+        (new AuthController())->logout();
+        break;
+
+    case "dashboard":
+        (new MaeController())->index();
+        break;
+
+    case "mae_create":
+        (new MaeController())->create();
+        break;
+
+    case "mae_store":
+        (new MaeController())->store();
+        break;
+
+    case "mae_edit":
+        (new MaeController())->edit();
+        break;
+
+    case "mae_update":
+        (new MaeController())->update();
+        break;
+
+    case "mae_delete":
+        (new MaeController())->delete();
         break;
 
     default:
-        $controller->login();
+        (new AuthController())->login();
 }
