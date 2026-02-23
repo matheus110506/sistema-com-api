35b8885e451b424c34c7638b9e095ab2894201d2
matheus110506 <matheus110506@hotmail.com>
2026-02-22 23:19:28 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once "controllers/AuthController.php";
+
+$controller = new AuthController();
+
+$page = $_GET["page"] ?? "login";
+
+switch ($page) {
+
+    case "login":
+        $controller->login();
+        break;
+
+    case "dashboard":
+        $controller->dashboard();
+        break;
+
+    case "logout":
+        $controller->logout();
+        break;
+
+    default:
+        $controller->login();
+}
