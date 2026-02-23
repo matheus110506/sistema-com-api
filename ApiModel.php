3db2ca3e38ae2ad0bb4a32bd0c589567c602e12a
matheus110506 <matheus110506@hotmail.com>
2026-02-22 23:19:57 -0300

    Refactor ApiModel for simplified login method

--- ApiModel.php
+++ ApiModel.php
@@ -4,47 +4,25 @@ class ApiModel {
 
     private $baseUrl = "http://localhost:3000";
 
-    private function request($endpoint, $method = "GET", $data = nill, $token = null) {
+    public function login($email, $senha) {
 
-        $ch = curl_init($this->baseUrl . $endpoint);
+        $dados = [
+            "email" => $email,
+            "senha" => $senha
+        ];
+
+        $ch = curl_init($this->baseUrl . "/login");
 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-
-        $headers = ['Content-Type: application/json'];
-
-        if ($token) {
-            $headers[] = "Authorization: Bearer $token";
-        }
-
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-        if ($data) {
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-        }
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json'
+        ]);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($dados));
 
         $response = curl_exec($ch);
         curl_close($ch);
 
         return json_decode($response, true);
-    }
-
-    public function login($email, $senha) {
-        return $this->request("/login", "POST", [
-            "email" => $email,
-            "senha" => $senha
-        ]);
-    }
-
-    public function cadastrar($nome, $email, $senha) {
-        return $this->request("/usuarios", "POST", [
-            "nome" => $nome,
-            "email" => $email,
-            "senha" => $senha
-        ]);
-    }
-
-    public function listarMaes($token) {
-        return $this->request("/maes", "GET", null, $token);
     }
 }
