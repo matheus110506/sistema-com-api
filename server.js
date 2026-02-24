3184b75d49a7ede8937e9990ebed6858321eb2da
matheus110506 <matheus110506@hotmail.com>
2026-02-24 19:17:00 -0300

    Refactor server.js to organize route imports

--- server.js
+++ server.js
@@ -9,8 +9,16 @@ app.use(express.json());
 
 require('./config/db');
 
+// Rotas
 const usuarioRoutes = require('./routes/usuarioRoutes');
+const maeRoutes = require('./routes/maeRoutes');
+const filhoRoutes = require('./routes/filhoRoutes');
+const tarefaRoutes = require('./routes/tarefaRoutes');
+
 app.use('/', usuarioRoutes);
+app.use('/', maeRoutes);
+app.use('/', filhoRoutes);
+app.use('/', tarefaRoutes);
 
 app.get('/', (req,res) => {
     res.send("API funcionando");
@@ -19,16 +27,3 @@ app.get('/', (req,res) => {
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
 });
-
-const maeRoutes = require('./routes/maeRoutes');
-app.use('/', maeRoutes);
-
-const filhoRoutes = require('./routes/filhoRoutes');
-app.use('/', filhoRoutes);
-
-const tarefaRoutes = require('./routes/tarefaRoutes');
-
-app.use('/', usuarioRoutes);
-app.use('/', maeRoutes);
-app.use('/', filhoRoutes);
-app.use('/', tarefaRoutes);
